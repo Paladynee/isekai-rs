813908240d81adcfaa338aa7a2f36dcb15a122f4
Voxell Paladynee <paladyneshadow@gmail.com>
2024-10-22 07:50:48 +0300

    the refactorination

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,14 @@
 //! i am system
 //! you are now being reincarnated... please standby...
 #![allow(unused)]
-#![allow(clippy::collapsible_match)]
 
 use std::{io, path::Path};
 
 use component::{
-    IsekaiCheatSystem, Menu, MenuHandler, Multiverse, TerminalInterface, Universe,
+    interface::TerminalInterface,
+    menu::{Menu, MenuHandler},
+    multiverse::Multiverse,
+    system::IsekaiCheatSystem,
     SAVEFILE_LOCATION,
 };
 use voxell_utils::time_seeded_rng::TimeSeededXorShift32;
@@ -16,16 +18,13 @@ use voxell_utils::time_seeded_rng::TimeSeededXorShift32;
 /// fabric of reality (lol)
 mod component;
 
-/// the outside force that is the player
-mod event_handler;
-
 /// big bang
 fn main() {
     let mut multiverse = Multiverse::new(
         IsekaiCheatSystem::load_save_from_file(Path::new(SAVEFILE_LOCATION)).unwrap_or_default(),
         TimeSeededXorShift32::new().unwrap(),
         TerminalInterface::new(io::stdout()).unwrap(),
-        MenuHandler::new(Menu::MainMenu),
+        MenuHandler::new(Menu::Main),
     );
 
     multiverse
